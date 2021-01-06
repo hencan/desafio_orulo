bc5de268a4df387629ee3200e4a720f647edcd6a
Henrique Candolo <hen.can@hotmail.com>
2021-01-05 23:10:42 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
+  
+  resources :favorites
+
+  # get "favorites/index"
+
+  # post "favorites/create"
+
+  # delete "favorite/destroy"
+  
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   get 'details/detail'
+
   devise_for :users
-  resources :favs
+
+  
   # get 'home/index'
 
   
-  get 'home/about'
-
-  get 'home/detail'
-
   root 'home#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
